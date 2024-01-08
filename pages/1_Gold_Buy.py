45d51ad674a8875404620b72cdebf988ef37aca0
Sayan <sayan10rakshit@gmail.com>
2024-01-08 22:31:02 +0530

    Added a descriptive info section in 'Buy Page'

--- pages/1_Gold_Buy.py
+++ pages/1_Gold_Buy.py
@@ -104,3 +104,56 @@ with st.container():
 
         st.markdown("### Total Price:")
         st.markdown(f"""## :green[₹ **{total_price:,.2f}**]""")
+
+    with st.expander("**Show Detailed Info. ℹ️**"):
+        col1, col2 = st.columns([1, 1], gap="small")
+        with col1:
+            st.markdown("- **:orange[Gold Charges]**:")
+        with col2:
+            st.markdown("The price of the gold in the ornament as per Carat.")
+            st.caption(
+                f"Here we have used **{carat} Carat** gold, whose price is **₹{hm_rate:,.2f}** per gram."
+            )
+
+        col1, col2 = st.columns([1, 1], gap="small")
+        with col1:
+            st.markdown("- **:green[Making Charges]**:")
+        with col2:
+            st.markdown(
+                'The cost of making the ornament, levied on the **"Gold Charges"** as percentage. The extra charges are also included in this.'
+            )
+            st.caption(
+                "Extra charges are the charges levied on the ornament for adding extra features like stones, conch bangles etc."
+            )
+            st.caption(
+                f"Here we have used **{making_charge_perc}%** making charges, which is **₹{making_charges:,.2f}** and extra charges of **₹{extra_charges:,.2f}**."
+            )
+
+        col1, col2 = st.columns([1, 1], gap="small")
+        with col1:
+            st.markdown("- **:green[Hallmark Charges]**:")
+        with col2:
+            st.markdown(
+                "The price of testing the purity of the ornament and assigning a unique ID (HUID) to the ornament."
+            )
+            st.caption("The standard hallmark charges are **₹45/pc.**.")
+
+        col1, col2 = st.columns([1, 1], gap="small")
+        with col1:
+            st.markdown("- **:green[Tax]**:")
+        with col2:
+            st.markdown(
+                "State Goods and Services Tax (SGST) & Central Goods and Services Tax (CGST). This tax is levied on **:orange[Gold Charges]** + **:green[Making Charges]** + **:green[Hallmark Charges]**."
+            )
+            st.caption("The standard GST rate is **3%**. (1.5% SGST + 1.5% CGST)")
+            st.caption(
+                f"Here we have used **1.5%** SGST {tax/2:,.2f} + **1.5%** CGST {tax/2:,.2f} = **₹{tax:,.2f}**."
+            )
+
+        col1, col2 = st.columns([1, 1], gap="small")
+        with col1:
+            st.markdown("- **:green[Total Price]**:")
+        with col2:
+            st.markdown(
+                "The final price of the ornament. This is **:orange[Gold Charges]** + **:green[Making Charges]** + **:green[Hallmark Charges]** + **:green[Tax]**."
+            )
