c324a78b35ed02978cd1f47aa906dc34ce4c745f
Sayan Rakshit <sayan10rakshit@gmail.com>
2025-04-19 12:41:36 +0530

    Update 2_Making_Charge.py
    Increased H.M. charges to 53

--- pages/2_Making_Charge.py
+++ pages/2_Making_Charge.py
@@ -24,7 +24,7 @@ with col1:
         "Enter the weight of gold (grams)", value=1.000, step=0.001, format="%.3f"
     )
     total_price = st.number_input("Enter the total price", value=7058.55)
-    hm_charges = st.number_input("Enter the Hallmark charges", value=45)
+    hm_charges = st.number_input("Enter the Hallmark charges", value=53)
     no_pcs = st.number_input(
         "Enter the number of ornament pieces", value=1, min_value=1
     )
